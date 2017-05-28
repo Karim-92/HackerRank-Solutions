7a722e601d057731f73ee8c698ddaa3f69504e93
Karim Ahmed <Karim.asu92@gmail.com>
2017-05-28 15:48:42 +0200

    Added simple array sum.

---
+++ Warmup/Simple Array Sum/main.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N=0;
+    int sum=0;
+    int temp=0;
+    cin>>x;
+    for(int i=0; i<N; i++){
+        cin>>temp;
+        sum=sum+temp;
+    }
+    cout<<sum;
+
+    return 0;
+}
